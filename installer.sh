39adfeb51abaedf46c2eee897f5db42989e2dae7
Bruno Dias <contact@diasbruno.com.br>
2013-06-09 22:16:21 -0300

    update installer.sh.

--- installer.sh
+++ installer.sh
@@ -1,7 +1,11 @@
+echo ""
 echo "Downloading flash-cli.zip from github..."
+echo ""
 curl "https://codeload.github.com/diasbruno/flash-cli/zip/master" >> flash-cli.zip
 echo "OK!"
+echo ""
 echo "Extracting..."
+echo ""
 tar -xzf flash-cli.zip
 rm flash-cli.zip
 echo "OK!"
